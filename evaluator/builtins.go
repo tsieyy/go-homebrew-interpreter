35db9ed3b1e0931f7c7f10ca9e5ed43e636304f6
tsieyy <tsieyy@qq.com>
2023-01-30 14:15:28 +0800

    finish base monkey

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,7 @@
 package evaluator
 
 import (
+	"fmt"
 	"monkey/object"
 )
 
@@ -92,6 +93,14 @@ var builtins = map[string]*object.Builtin {
 			copy(newElements, arr.Elements) 
 			newElements[length] = args[1] 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
 		},
 	},
 } 
